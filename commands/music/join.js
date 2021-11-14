04773b4397e3263142034a55cc0322c34e2743ee
maskiilovmai <maikimlong2405@gmail.com>
2021-11-14 17:09:40 +0700

    add join command

--- commands/music/join.js
+++ commands/music/join.js
@@ -4,13 +4,7 @@ module.exports = {
     aliases: ['j'],
     category: 'Music',
     utilisation: '{prefix}join',
-    // run: async (client, message, args, { GuildDB }) => {
 
-    //     if (!message.member.voice.channel) return; //u cant try disconnect without entering a vc
-
-    //     message.member.voice.channel.leave(); //Leave the voice channel
-    //     message.channel.send("Đã cút khỏi kênh thoại!")
-    // }
     execute(client, message) {
         if (!message.member.voice.channel) 
         return message.channel.send("Vui lòng tham gia kênh thoại trước");
